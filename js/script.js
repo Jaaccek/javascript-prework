88ee70755a22b42a8beca124f9663f17d3871220
Jacek Knapczyk <jacekknapczyk@gmail.com>
2021-05-19 19:42:52 +0200

    computerMove

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
-printMessage('Zagrałem kamień! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem ' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-let computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!');
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} 
+
+if(computerInput == '2'){
+    computerMove = 'papier'
+}
+
+if(computerInput == '3'){
+    computerMove = 'nożyce'
+}
+printMessage('Twój ruch to: ' + playerMove); 

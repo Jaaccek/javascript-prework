8649b3d61f080a1a7484340787966e6c2814f4ab
Jacek Knapczyk <jacekknapczyk@gmail.com>
2021-05-23 16:26:36 +0200

    change getMoveName

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
+function getMoveName(randomNumber){
+    
+    if( randomNumber == 1){
+      return 'kamień';
+    }
+    else if( randomNumber == 2){
+        return 'papier';
+    }
+    else( randomNumber == 3);{
+        return 'nożyce';
+    }
+}     
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
+/*if(randomNumber == 1){
   computerMove = 'kamień';
 }
 else if(randomNumber == 2){
@@ -13,6 +26,7 @@ else if(randomNumber == 2){
 else if(randomNumber == 3){
     computerMove = 'nożyce';
 }
+*/
 
 printMessage('Mój ruch to: ' + computerMove);
 
@@ -20,8 +34,9 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove =  getMoveName(randomNumber);
 
+/*
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
@@ -34,6 +49,7 @@ else if(playerInput == '3'){
 else {
     playerMove = 'Nieznany ruch';
 }
+*/
 printMessage('Twój ruch to: ' + playerMove);
 
 if(playerMove == 'Nieznany ruch'){
